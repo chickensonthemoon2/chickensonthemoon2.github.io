1c9d4a9dd593e0a93cfdcd3fc1cf8d5c66f71ec4
chickensonthemoon2 <annarosesewart@gmail.com>
2015-08-23 19:05:26 +0100

    Blank scores

---
+++ dino.js
@@ -0,0 +1,151 @@
+var points = [{
+  'question': 'Which colour?',
+  'answer' : {
+    'Purple':{
+      'PurpleDino':10, 'UniDino':4, 'EvilDino':5, 'RandomDino':2, 'WetDino':5
+    }
+    'Green':{
+      'PurpleDino':2, 'UniDino':4, 'EvilDino':3, 'RandomDino':5, 'WetDino':4
+    }
+    'Blue':{
+      'PurpleDino':5, 'UniDino':4, 'EvilDino':2, 'RandomDino':5, 'WetDino':10
+    }
+  }
+},
+
+{
+  'question': 'Do you like pineapples?',
+  'answer' : {
+    'Yes':{
+      'PurpleDino':5, 'UniDino':4, 'EvilDino':0, 'RandomDino':10, 'WetDino':2
+    }
+    'No':{
+      'PurpleDino':5, 'UniDino':6, 'EvilDino':10, 'RandomDino':0, 'WetDino':8
+    }
+    'Kinda':{
+      'PurpleDino':10, 'UniDino':5, 'EvilDino':0, 'RandomDino':0, 'WetDino':2
+    }
+  }
+},
+
+{
+  'question': 'What is your favourite lesson at school?',
+  'answer' : {
+    'Maths':{
+      'PurpleDino':99, 'UniDino':99, 'EvilDino':99, 'RandomDino':99, 'WetDino':99
+    }
+    'PE':{
+      'PurpleDino'99, 'UniDino'99, 'EvilDino':99, 'RandomDino':99, 'WetDino':99
+    }
+    'I hate school':{
+      'PurpleDino':99, 'UniDino':99, 'EvilDino':99, 'RandomDino':99, 'WetDino':99
+    }
+  }
+},
+
+{
+  'question': 'What is your favourite food?',
+  'answer' : {
+    'Curry':{
+      'PurpleDino':99, 'UniDino':99, 'EvilDino':99, 'RandomDino':99, 'WetDino':99
+    }
+    'Pizza':{
+      'PurpleDino'99, 'UniDino'99, 'EvilDino':99, 'RandomDino':99, 'WetDino':99
+    }
+    'Yummy food':{
+      'PurpleDino':99, 'UniDino':99, 'EvilDino':99, 'RandomDino':99, 'WetDino':99
+    }
+  }
+},
+
+{
+  'question': 'Which animals?',
+  'answer' : {
+    'Unicorns':{
+      'PurpleDino':99, 'UniDino':99, 'EvilDino':99, 'RandomDino':99, 'WetDino':99
+    }
+    'Black Cats':{
+      'PurpleDino'99, 'UniDino'99, 'EvilDino':99, 'RandomDino':99, 'WetDino':99
+    }
+    'Dolphins':{
+      'PurpleDino':99, 'UniDino':99, 'EvilDino':99, 'RandomDino':99, 'WetDino':99
+    }
+  }
+},
+
+{
+  'question': 'Which sky object?',
+  'answer' : {
+    'Rainbows':{
+      'PurpleDino':99, 'UniDino':99, 'EvilDino':99, 'RandomDino':99, 'WetDino':99
+    }
+    'The Sun':{
+      'PurpleDino'99, 'UniDino'99, 'EvilDino':99, 'RandomDino':99, 'WetDino':99
+    }
+    'Rain':{
+      'PurpleDino':99, 'UniDino':99, 'EvilDino':99, 'RandomDino':99, 'WetDino':99
+    }
+  }
+},
+
+{
+  'question': 'Do you say random words?',
+  'answer' : {
+    'Yes':{
+      'PurpleDino':99, 'UniDino':99, 'EvilDino':99, 'RandomDino':99, 'WetDino':99
+    }
+    'No':{
+      'PurpleDino'99, 'UniDino'99, 'EvilDino':99, 'RandomDino':99, 'WetDino':99
+    }
+    'POTATOES!':{
+      'PurpleDino':99, 'UniDino':99, 'EvilDino':99, 'RandomDino':99, 'WetDino':99
+    }
+  }
+},
+
+{
+  'question': 'Which?',
+  'answer' : {
+    'Air':{
+      'PurpleDino':99, 'UniDino':99, 'EvilDino':99, 'RandomDino':99, 'WetDino':99
+    }
+    'Water':{
+      'PurpleDino'99, 'UniDino'99, 'EvilDino':99, 'RandomDino':99, 'WetDino':99
+    }
+    'Lava (Evil laugh)':{
+      'PurpleDino':99, 'UniDino':99, 'EvilDino':99, 'RandomDino':99, 'WetDino':99
+    }
+  }
+},
+
+{
+  'question': 'What are you on whichunicorn.com?',
+  'answer' : {
+    'Not done it yet':{
+      'PurpleDino':99, 'UniDino':99, 'EvilDino':99, 'RandomDino':99, 'WetDino':99
+    }
+    'Ninjacorn':{
+      'PurpleDino'99, 'UniDino'99, 'EvilDino':99, 'RandomDino':99, 'WetDino':99
+    }
+    'Other':{
+      'PurpleDino':99, 'UniDino':99, 'EvilDino':99, 'RandomDino':99, 'WetDino':99
+    }
+  }
+},
+
+{
+  'question': 'Are you a unicorn?',
+  'answer' : {
+    'Yes':{
+      'PurpleDino':99, 'UniDino':99, 'EvilDino':99, 'RandomDino':99, 'WetDino':99
+    }
+    'No':{
+      'PurpleDino'99, 'UniDino'99, 'EvilDino':99, 'RandomDino':99, 'WetDino':99
+    }
+    'I want to be':{
+      'PurpleDino':99, 'UniDino':99, 'EvilDino':99, 'RandomDino':99, 'WetDino':99
+    }
+  }
+},
+
+];
